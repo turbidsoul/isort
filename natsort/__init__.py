7692aef4adc3f02a82c058e00610b385daa13f1b
Turbidsoul <sccn.sq@gmail.com>
2015-07-16 10:02:28 +0800

    update isort to 4.0.0

--- natsort/__init__.py
+++
@@ -1,9 +0,0 @@
-from .natsort import natsort_key, natsorted, index_natsorted
-from ._version import __version__
-
-__all__ = [
-           'natsort_key',
-           'natsorted',
-           'index_natsorted',
-          ]
-
--- pies/functools.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-from functools import *
-
-from .version_info import PY2
-
-if PY2:
-    reduce = reduce
